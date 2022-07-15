033787da4da4e6e378e3eddfe4c56c617bc9723b
Shockch4rge <favouriteteo@gmail.com>
2022-07-16 05:05:22 +0800

    Use router outlets for sections of screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,60 @@
 import "./assets/styles/index.css";
 
 import { useCallback, useEffect } from "react";
-import { DragDropContext, DropResult } from "react-beautiful-dnd";
-import { useRoutes } from "react-router-dom";
+import { Route, Routes, useRoutes } from "react-router-dom";
+
+import { Center, Heading, Spinner, VStack, Wrap } from "@chakra-ui/react";
 
 import { useGetUserBoardsQuery, useLazyGetUserBoardsQuery } from "./app/services/boards";
 import { setCurrentBoard } from "./app/slices/board";
+import { Toast } from "./components/Toast";
 import { BoardPage } from "./features/board/ui/BoardPage";
-import { HomePage } from "./features/home/ui/HomePage";
+import { LandingPage } from "./features/landing/ui/LandingPage";
+import { MainView } from "./features/main/ui/MainView";
+import { SettingsPage } from "./features/settings/SettingsPage";
 import { useAppDispatch } from "./hooks/useAppDispatch";
 import { useAppSelector } from "./hooks/useAppSelector";
 
 
 const App: React.FC = () => {
 	const dispatch = useAppDispatch();
-	
+	const currentBoard = useAppSelector(state => state.boards.current);
+	const { data: boards } = useGetUserBoardsQuery("939264bb-6cc6-46a6-aaad-bbb3dca9fcf5");
 
-	const onDragEnd = useCallback((result: DropResult) => {
-		//
+	useEffect(() => {
+		if (!boards) return;
+		dispatch(setCurrentBoard(boards[0]));
+	}, [boards]);
+
+	useEffect(() => {
+		if (!currentBoard) return;
+		document.title = `${currentBoard.name} | Flow`;
 	}, []);
-
-	const routes = useRoutes([
-		{
-			path: "/",
-			element: <HomePage />,
-		},
-		{
-			path: "/boards",
-			element: <BoardPage />,
-		},
-	]);
 
 	return (
 		<>
-			<DragDropContext onDragEnd={onDragEnd}>{routes}</DragDropContext>
+			<Routes>
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/home" element={<MainView />}>
+					<Route
+						path="board"
+						element={
+							currentBoard ? (
+								<BoardPage board={currentBoard} />
+							) : (
+								<Center>
+									<VStack>
+										<Spinner />
+										<Heading>Loading your boards...</Heading>
+									</VStack>
+								</Center>
+							)
+						}
+					/>
+					<Route path="settings" element={<SettingsPage />} />
+				</Route>
+			</Routes>
+			<Toast />
 		</>
 	);
 };
