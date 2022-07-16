b0de01085b623cba9336924ec8271efa27e1f05b
Shockch4rge <favouriteteo@gmail.com>
2022-07-17 01:52:59 +0800

    Delete cards

--- src/features/board/ui/components/EditCardModal.tsx
+++ src/features/board/ui/components/EditCardModal.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
-import { FaCross, FaPlus, FaTimes, FaXing } from "react-icons/fa";
+import { useMemo, useState } from "react";
+import {
+    FaCheckSquare, FaCross, FaPlus, FaStickyNote, FaTimes, FaTrash, FaXing
+} from "react-icons/fa";
 
 import {
-    AlertDialog, Box, Button, Editable, EditableInput, EditablePreview, Heading, HStack, IconButton,
-    Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay,
-    Tag, TagCloseButton, TagRightIcon, Text, Tooltip, useColorModeValue, Wrap, WrapItem
+    Box, Button, Editable, EditableInput, EditablePreview, EditableTextarea, Flex, Heading, HStack,
+    IconButton, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader,
+    ModalOverlay, Spinner, Tag, TagCloseButton, TagRightIcon, Text, Tooltip, VStack, Wrap, WrapItem
 } from "@chakra-ui/react";
 
+import { useDeleteCardMutation } from "../../../../app/services/cards";
 import { closeModal } from "../../../../app/slices/ui/modals";
 import { useAppDispatch } from "../../../../hooks/useAppDispatch";
 import { useAppSelector } from "../../../../hooks/useAppSelector";
@@ -16,94 +19,154 @@ const modalName = "editCard";
 
 export const EditCardModal: React.FC = () => {
 	const dispatch = useAppDispatch();
-
-	const [isAddingTag, setIsAddingTag] = useState(false);
 	const { open, target } = useAppSelector(state => state.ui.modals[modalName]);
+	const [deleteCard, { isLoading: isDeletingCard, isSuccess: hasDeletedCard }] = useDeleteCardMutation();
+	const [isAddingTagMode, setIsAddingTagMode] = useState(false);
 
 	const close = () => void dispatch(closeModal(modalName));
+
 	const handleAddTag = (tagName: string) => {
 		if (tagName.length === 0) {
-			setIsAddingTag(false);
+			setIsAddingTagMode(false);
 			return;
 		}
 
-		setIsAddingTag(false);
+		setIsAddingTagMode(false);
 	};
 
+	const handleDeleteCard = async () => {
+		if (!target) return;
+
+		await deleteCard(target.id);
+
+		close();
+	};
+
+	const componentButtons = useMemo(() => {
+		return [
+			{
+				icon: <FaCheckSquare />,
+				name: "Checklist",
+				onClick: () => {},
+			},
+			{
+				icon: <FaStickyNote />,
+				name: "Notepad",
+				onClick: () => {},
+			},
+		];
+	}, []);
+
 	return (
-		<Modal isOpen={open} onClose={close} size="2xl">
+		<Modal isOpen={open} onClose={close} size="6xl" closeOnOverlayClick={false} autoFocus={false}>
 			<ModalOverlay />
 			<ModalContent>
-				<ModalHeader>
-					<Editable defaultValue={target?.name} isPreviewFocusable selectAllOnFocus>
-						<Tooltip label="Click to edit">
-							<EditablePreview
-								w="100%"
-								py={2}
-								px={2}
-								_hover={{
-									bg: "gray.100",
-								}}
-							/>
-						</Tooltip>
-						<Input py={2} px={4} as={EditableInput} />
-					</Editable>
+				<ModalHeader as={HStack}>
+					<Tooltip label="Click to edit" hasArrow openDelay={1000}>
+						<Editable
+							flex="1"
+							defaultValue={target?.name}
+							borderRadius="6"
+							isPreviewFocusable
+							selectAllOnFocus
+							_hover={{
+								bg: "gray.100",
+							}}
+						>
+							<EditablePreview py={2} px={2} w="100%" />
+							<Input py={2} px={4} as={EditableInput} />
+						</Editable>
+					</Tooltip>
+					<IconButton aria-label="Delete card" onClick={handleDeleteCard}>
+						{isDeletingCard || hasDeletedCard ? <Spinner /> : <FaTrash />}
+					</IconButton>
 				</ModalHeader>
-				
-				<ModalBody>
-					<Heading size="md">Labels</Heading>
-					<Wrap mt="4">
-						<WrapItem>
-							<Tag bg="green.100">
-								Tech
-								<TagCloseButton />
-							</Tag>
-						</WrapItem>
-						{isAddingTag && (
-							<WrapItem>
-								<Editable
-									fontSize={"sm"}
-									defaultValue="New tag"
-									isPreviewFocusable={true}
-									selectAllOnFocus={true}
-									startWithEditView={true}
-									onSubmit={handleAddTag}
-								>
-									<EditablePreview
-										py={1}
-										px={1}
-										bg="gray.50"
-										_hover={{
-											bg: "gray.100",
-										}}
-									/>
-									<Input py={2} px={4} as={EditableInput} />
-								</Editable>
-							</WrapItem>
-						)}
-						<WrapItem>
-							{isAddingTag ? (
-								<IconButton
-									size="xs"
-									aria-label="Cancel"
-									icon={<FaTimes opacity="50%" />}
-									onClick={() => setIsAddingTag(false)}
-								/>
-							) : (
-								<IconButton
-									size="xs"
-									aria-label="Add tag"
-									icon={<FaPlus opacity="50%" />}
-									onClick={() => setIsAddingTag(true)}
-								/>
-							)}
-						</WrapItem>
-					</Wrap>
 
-					<Box mt="6">
-						<Heading size="md">Description</Heading>
-						<Text mt="4">{target?.description}</Text>
-					</Box>
+				<ModalBody>
+					<Flex>
+						<VStack flex="3" align="start">
+							<Heading size="md">Labels</Heading>
+							<Wrap mt="4">
+								<WrapItem>
+									<Tag bg="green.100">
+										Tech
+										<TagCloseButton />
+									</Tag>
+								</WrapItem>
+								{isAddingTagMode && (
+									<WrapItem>
+										<Editable
+											fontSize={"sm"}
+											defaultValue="New tag"
+											isPreviewFocusable={true}
+											selectAllOnFocus={true}
+											startWithEditView={true}
+											onSubmit={handleAddTag}
+										>
+											<EditablePreview
+												py={1}
+												px={1}
+												bg="gray.50"
+												_hover={{
+													bg: "gray.100",
+												}}
+											/>
+											<Input py={2} px={4} as={EditableInput} />
+										</Editable>
+									</WrapItem>
+								)}
+								<WrapItem>
+									{isAddingTagMode ? (
+										<IconButton
+											size="xs"
+											aria-label="Cancel"
+											icon={<FaTimes opacity="50%" />}
+											onClick={() => setIsAddingTagMode(false)}
+										/>
+									) : (
+										<IconButton
+											size="xs"
+											aria-label="Add tag"
+											icon={<FaPlus opacity="50%" />}
+											onClick={() => setIsAddingTagMode(true)}
+										/>
+									)}
+								</WrapItem>
+							</Wrap>
+
+							<Box mt="6">
+								<Heading size="md">Description</Heading>
+								<Editable
+									noOfLines={3}
+									defaultValue={
+										target?.description === ""
+											? "No description provided."
+											: target?.description
+									}
+								>
+									<EditablePreview w="100%" />
+									<EditableTextarea />
+								</Editable>
+							</Box>
+						</VStack>
+						<VStack flex="1" align="start">
+							<Heading size="md">Components</Heading>
+							<VStack w="100%" mt="4" p="2" borderRadius="6" bgColor="gray.100" align="center">
+								{componentButtons.map(({ icon, name, onClick }) => (
+									<Button
+										key={name}
+										w="100%"
+										bg="white"
+										leftIcon={icon}
+										fontWeight="medium"
+										onClick={onClick}
+									>
+										{name}
+									</Button>
+								))}
+							</VStack>
+						</VStack>
+					</Flex>
 				</ModalBody>
 
 				<ModalFooter>
@@ -113,8 +176,10 @@ export const EditCardModal: React.FC = () => {
 							bgColor="green.200"
 							textColor="green.700"
 							_hover={{ bgColor: "green.300" }}
+							disabled={isDeletingCard || hasDeletedCard}
 							_active={{ bgColor: "green.400", textColor: "green.900" }}
 							onClick={() => {
+								// save edits
 								close();
 							}}
 						>
