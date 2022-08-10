3c99cfda67b11c9a81b938980107c0235393d7ee
Shockch4rge <favouriteteo@gmail.com>
2022-08-11 00:18:40 +0800

    Attempt at polling interval for refetching boards in case of error

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import "./assets/styles/index.css";
+// import "./assets/styles/index.css";
 
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Route, Routes, useRoutes } from "react-router-dom";
 
-import { Center, Heading, Spinner, VStack, Wrap } from "@chakra-ui/react";
+import { Box, Center, Heading, Spinner, Text, VStack, Wrap } from "@chakra-ui/react";
 
 import { useGetUserBoardsQuery, useLazyGetUserBoardsQuery } from "./app/services/boards";
 import { setCurrentBoard } from "./app/slices/board";
-import { Toast } from "./components/Toast";
+import { Toast } from "./common-components/Toast";
 import { BoardPage } from "./features/board/ui/BoardPage";
 import { LandingPage } from "./features/landing/ui/LandingPage";
 import { MainView } from "./features/main/ui/MainView";
-import { SettingsPage } from "./features/settings/SettingsPage";
+import { SettingsPage } from "./features/settings/ui/SettingsPage";
 import { useAppDispatch } from "./hooks/useAppDispatch";
 import { useAppSelector } from "./hooks/useAppSelector";
-
+import { MOCK_USER_ID } from "./utils/mockData";
 
 const App: React.FC = () => {
 	const dispatch = useAppDispatch();
+	const [pollingInterval, setPollingInterval] = useState(0);
 	const currentBoard = useAppSelector(state => state.boards.current);
-	const { data: boards } = useGetUserBoardsQuery("939264bb-6cc6-46a6-aaad-bbb3dca9fcf5");
+	const {
+		data: boards,
+		isLoading: isLoadingBoards,
+		isSuccess: hasFetchedBoards,
+	} = useGetUserBoardsQuery(MOCK_USER_ID);
+
+	useEffect(() => {
+		if (!isLoadingBoards || hasFetchedBoards) return;
+		setTimeout(() => setPollingInterval(v => v + 10000), 10000);
+	}, [isLoadingBoards, hasFetchedBoards]);
 
 	useEffect(() => {
 		if (!boards) return;
-		dispatch(setCurrentBoard(boards[0]));
+		dispatch(setCurrentBoard(boards[+localStorage.getItem("lastEditedBoard")! ?? 0]));
 	}, [boards]);
 
 	useEffect(() => {
 		if (!currentBoard) return;
 		document.title = `${currentBoard.name} | Flow`;
-	}, []);
+	}, [currentBoard]);
 
 	return (
 		<>
@@ -42,10 +52,16 @@ const App: React.FC = () => {
 							currentBoard ? (
 								<BoardPage board={currentBoard} />
 							) : (
-								<Center>
+								<Center w="full">
 									<VStack>
 										<Spinner />
 										<Heading>Loading your boards...</Heading>
+										{pollingInterval > 0 && (
+											<Text>
+												We're having trouble at the moment. Refetching in{" "}
+												{pollingInterval / 1000} seconds.
+											</Text>
+										)}
 									</VStack>
 								</Center>
 							)
