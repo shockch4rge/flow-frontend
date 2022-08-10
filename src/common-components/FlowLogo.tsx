51a26403a995d3f621ae946abd58178902fed2d5
Shockch4rge <favouriteteo@gmail.com>
2022-08-10 14:13:00 +0800

    Added configuration options to logo

--- src/common-components/FlowLogo.tsx
+++ src/common-components/FlowLogo.tsx
@@ -1,20 +1,52 @@
 import { Heading, HStack, Image } from "@chakra-ui/react";
 
-
 interface Props {
 	size: number;
+	variant?: "logo" | "text" | "full";
+	color?: "dark" | "light";
 }
 
-export const FlowLogo: React.FC<Props> = props => {
+export const FlowLogo: React.FC<Props> = ({ size, variant, color }) => {
+	const logo = (
+		<Image
+			boxSize={`${size}px`}
+			src="/src/assets/flow-logo.svg"
+			alt="Flow logo"
+			userSelect="none"
+		/>
+	);
+
+	const text = (
+		<Heading
+			userSelect="none"
+			fontFamily="Lexend"
+			textColor={color === "dark" ? "black" : "white"}
+		>
+			Flow
+		</Heading>
+	);
+
+	if (variant === "logo") {
+		return logo;
+	}
+
+	if (variant === "text") {
+		return text;
+	}
+
 	return (
 		<HStack>
 			<Image
-				boxSize={`${props.size}px`}
+				boxSize={`${size}px`}
 				src="/src/assets/flow-logo.svg"
 				alt="Flow logo"
 				userSelect="none"
 			/>
-			<Heading userSelect="none" fontFamily="Lexend">
+			<Heading
+				userSelect="none"
+				fontFamily="Lexend"
+				textColor={color === "dark" ? "black" : "white"}
+			>
 				Flow
 			</Heading>
 		</HStack>
