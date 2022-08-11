fb705c9edffb982dad8f2f9cf284c14b1b797c66
Shockch4rge <favouriteteo@gmail.com>
2022-08-11 11:33:45 +0800

    Formatting

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,6 @@ import App from "./App";
 import store from "./app/store";
 import theme from "./utils/theme";
 
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<Provider store={store}>
