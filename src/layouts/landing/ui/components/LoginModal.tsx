1e7845640671e2f03ab99e88cd602a25225a6989
Favian <favouriteteo@gmail.com>
2022-08-19 21:34:50 +0800

    Formatting

--- src/layouts/landing/ui/components/LoginModal.tsx
+++ src/layouts/landing/ui/components/LoginModal.tsx
@@ -5,33 +5,10 @@ import { Link as RouterLink, useNavigate } from "react-router-dom";
 import * as Yup from "yup";
 
 import {
-	Box,
-	Button,
-	ButtonGroup,
-	Flex,
-	FormControl,
-	FormErrorMessage,
-	FormLabel,
-	Heading,
-	Hide,
-	HStack,
-	IconButton,
-	Image,
-	Input,
-	InputGroup,
-	InputRightElement,
-	Link,
-	Modal,
-	ModalBody,
-	ModalCloseButton,
-	ModalContent,
-	ModalFooter,
-	ModalHeader,
-	ModalOverlay,
-	Spinner,
-	Text,
-	Tooltip,
-	VStack,
+    Box, Button, ButtonGroup, Flex, FormControl, FormErrorMessage, FormLabel, Heading, Hide, HStack,
+    IconButton, Image, Input, InputGroup, InputRightElement, Link, Modal, ModalBody,
+    ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Spinner, Text, Tooltip,
+    VStack
 } from "@chakra-ui/react";
 
 import { closeModal, openModal } from "../../../../app/slices/ui/modals";
@@ -45,169 +22,169 @@ const emailFieldName = "email";
 const passwordFieldName = "password";
 
 export const LoginModal: React.FC = () => {
-	const navigate = useNavigate();
-	const dispatch = useAppDispatch();
-	const { user, loginUser } = useAuthContext();
-	const [showPassword, setShowPassword] = useState(false);
-	const { open } = useAppSelector(state => state.ui.modals.login);
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
+    const { user, loginUser } = useAuthContext();
+    const [showPassword, setShowPassword] = useState(false);
+    const { open } = useAppSelector(state => state.ui.modals.login);
 
-	const close = useCallback(() => dispatch(closeModal(loginModalName)), []);
+    const close = useCallback(() => dispatch(closeModal(loginModalName)), []);
 
-	return (
-		<Modal size={["xs", "md", "lg", "xl"]} isCentered isOpen={open} onClose={close}>
-			<ModalOverlay />
-			<ModalContent>
-				<ModalHeader>
-					<Flex justify="center">
-						<FlowLogo size={50} />
-					</Flex>
-				</ModalHeader>
-				<ModalCloseButton />
-				<Formik
-					validationSchema={LoginSchema}
-					initialValues={{
-						[emailFieldName]: "",
-						[passwordFieldName]: "",
-					}}
-					onSubmit={async ({ email, password }) => {
-						await loginUser({
-							email,
-							password,
-						});
-						close();
-					}}
-				>
-					{({ errors, touched, isSubmitting, isValid, getFieldProps }) => (
-						<ModalBody>
-							<Heading textAlign="center" size="lg">
-								Login <Hide below="md">to your account</Hide>
-							</Heading>
+    return (
+        <Modal size={["xs", "md", "lg", "xl"]} isCentered isOpen={open} onClose={close}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>
+                    <Flex justify="center">
+                        <FlowLogo size={50} />
+                    </Flex>
+                </ModalHeader>
+                <ModalCloseButton />
+                <Formik
+                    validationSchema={LoginSchema}
+                    initialValues={{
+                        [emailFieldName]: "",
+                        [passwordFieldName]: "",
+                    }}
+                    onSubmit={async ({ email, password }) => {
+                        await loginUser({
+                            email,
+                            password,
+                        });
+                        close();
+                    }}
+                >
+                    {({ errors, touched, isSubmitting, isValid, getFieldProps }) => (
+                        <ModalBody>
+                            <Heading textAlign="center" size="lg">
+                                Login <Hide below="md">to your account</Hide>
+                            </Heading>
 
-							<Form autoComplete="off">
-								<VStack mt="10" spacing="6" justify="center">
-									<Field name={emailFieldName}>
-										{(props: any) => (
-											<FormControl
-												isInvalid={!!errors.email && touched.email}
-											>
-												<FormLabel htmlFor={emailFieldName}>
-													Email
-												</FormLabel>
-												<Input
-													{...getFieldProps(emailFieldName)}
-													id={emailFieldName}
-													placeholder="e.g. johndoe@gmail.com"
-												/>
-												<FormErrorMessage>{errors.email}</FormErrorMessage>
-											</FormControl>
-										)}
-									</Field>
+                            <Form>
+                                <VStack mt="10" spacing="6" justify="center">
+                                    <Field name={emailFieldName}>
+                                        {(props: any) => (
+                                            <FormControl
+                                                isInvalid={!!errors.email && touched.email}
+                                            >
+                                                <FormLabel htmlFor={emailFieldName}>
+                                                    Email
+                                                </FormLabel>
+                                                <Input
+                                                    {...getFieldProps(emailFieldName)}
+                                                    id={emailFieldName}
+                                                    placeholder="e.g. johndoe@gmail.com"
+                                                />
+                                                <FormErrorMessage>{errors.email}</FormErrorMessage>
+                                            </FormControl>
+                                        )}
+                                    </Field>
 
-									<Field name={passwordFieldName}>
-										{(props: any) => (
-											<FormControl
-												isInvalid={!!errors.password && touched.password}
-											>
-												<Text
-													mb="-6"
-													textColor="green.400"
-													textDecor="underline"
-													textAlign="end"
-													fontSize="sm"
-													cursor="pointer"
-													onClick={() => {
-														close();
-														setTimeout(() => {
-															dispatch(openModal("resetPassword"));
-														}, 300);
-													}}
-												>
-													Forgot your password?
-												</Text>
-												<FormLabel htmlFor={passwordFieldName}>
-													Password
-												</FormLabel>
-												<InputGroup>
-													<Input
-														{...getFieldProps(passwordFieldName)}
-														id={passwordFieldName}
-														type={showPassword ? "text" : "password"}
-														placeholder="Password"
-													/>
-													<Tooltip
-														label="Toggle Visibility"
-														openDelay={1000}
-														hasArrow
-													>
-														<InputRightElement
-															onClick={() =>
-																setShowPassword(!showPassword)
-															}
-														>
-															<IconButton
-																size="sm"
-																bg="transparent"
-																aria-label="Toggle password visibility"
-															>
-																{showPassword ? (
-																	<FaEye size="20" />
-																) : (
-																	<FaEyeSlash size="20" />
-																)}
-															</IconButton>
-														</InputRightElement>
-													</Tooltip>
-												</InputGroup>
-												<FormErrorMessage>
-													{errors.password}
-												</FormErrorMessage>
-											</FormControl>
-										)}
-									</Field>
-								</VStack>
+                                    <Field name={passwordFieldName}>
+                                        {(props: any) => (
+                                            <FormControl
+                                                isInvalid={!!errors.password && touched.password}
+                                            >
+                                                <Text
+                                                    mb="-6"
+                                                    textColor="green.400"
+                                                    textDecor="underline"
+                                                    textAlign="end"
+                                                    fontSize="sm"
+                                                    cursor="pointer"
+                                                    onClick={() => {
+                                                        close();
+                                                        setTimeout(() => {
+                                                            dispatch(openModal("resetPassword"));
+                                                        }, 300);
+                                                    }}
+                                                >
+                                                    Forgot your password?
+                                                </Text>
+                                                <FormLabel htmlFor={passwordFieldName}>
+                                                    Password
+                                                </FormLabel>
+                                                <InputGroup>
+                                                    <Input
+                                                        {...getFieldProps(passwordFieldName)}
+                                                        id={passwordFieldName}
+                                                        type={showPassword ? "text" : "password"}
+                                                        placeholder="Password"
+                                                    />
+                                                    <Tooltip
+                                                        label="Toggle Visibility"
+                                                        openDelay={1000}
+                                                        hasArrow
+                                                    >
+                                                        <InputRightElement
+                                                            onClick={() =>
+                                                                setShowPassword(!showPassword)
+                                                            }
+                                                        >
+                                                            <IconButton
+                                                                size="sm"
+                                                                bg="transparent"
+                                                                aria-label="Toggle password visibility"
+                                                            >
+                                                                {showPassword ? (
+                                                                    <FaEye size="20" />
+                                                                ) : (
+                                                                    <FaEyeSlash size="20" />
+                                                                )}
+                                                            </IconButton>
+                                                        </InputRightElement>
+                                                    </Tooltip>
+                                                </InputGroup>
+                                                <FormErrorMessage>
+                                                    {errors.password}
+                                                </FormErrorMessage>
+                                            </FormControl>
+                                        )}
+                                    </Field>
+                                </VStack>
 
-								<Button
-									mt="12"
-									w="full"
-									type="submit"
-									variant="primary"
-									disabled={
-										!isValid ||
-										isSubmitting ||
-										!touched.email ||
-										!touched.password
-									}
-								>
-									{isSubmitting ? <Spinner /> : "Login"}
-								</Button>
-							</Form>
+                                <Button
+                                    mt="12"
+                                    w="full"
+                                    type="submit"
+                                    variant="primary"
+                                    disabled={
+                                        !isValid ||
+                                        isSubmitting ||
+                                        !touched.email ||
+                                        !touched.password
+                                    }
+                                >
+                                    {isSubmitting ? <Spinner /> : "Login"}
+                                </Button>
+                            </Form>
 
-							<Text
-								my="8"
-								textAlign="center"
-								textDecor="underline"
-								cursor="pointer"
-								onClick={() => {
-									close();
-									setTimeout(() => {
-										dispatch(openModal("signup"));
-									}, 300);
-								}}
-							>
-								<a>Create Account</a>
-							</Text>
-						</ModalBody>
-					)}
-				</Formik>
-			</ModalContent>
-		</Modal>
-	);
+                            <Text
+                                my="8"
+                                textAlign="center"
+                                textDecor="underline"
+                                cursor="pointer"
+                                onClick={() => {
+                                    close();
+                                    setTimeout(() => {
+                                        dispatch(openModal("signup"));
+                                    }, 300);
+                                }}
+                            >
+                                <a>Create Account</a>
+                            </Text>
+                        </ModalBody>
+                    )}
+                </Formik>
+            </ModalContent>
+        </Modal>
+    );
 };
 
 const LoginSchema = Yup.object().shape({
-	[emailFieldName]: Yup.string()
-		.required()
-		.email("Invalid email.")
-		.required("Email is required."),
-	[passwordFieldName]: Yup.string().required("Password is required."),
+    [emailFieldName]: Yup.string()
+        .required()
+        .email("Invalid email.")
+        .required("Email is required."),
+    [passwordFieldName]: Yup.string().required("Password is required."),
 });
