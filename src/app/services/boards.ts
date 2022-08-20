186d899de068360ab04c1699b46ca95e8b59b328
Favian <favouriteteo@gmail.com>
2022-08-21 02:07:59 +0800

    More automated re-fetching

--- src/app/services/boards.ts
+++ src/app/services/boards.ts
@@ -3,104 +3,106 @@ import { iBoard } from "../../utils/models";
 import api, { ApiTags } from "./api";
 
 const boardService = api.injectEndpoints({
-	endpoints: builder => ({
-		getUserBoards: builder.query<iBoard[], iBoard["authorId"]>({
-			query: userId => ({
-				url: `/users/${userId}/boards`,
-				method: "GET",
-			}),
+    endpoints: builder => ({
+        getUserBoards: builder.query<iBoard[], iBoard["authorId"]>({
+            query: userId => ({
+                url: `/users/${userId}/boards`,
+                method: "GET",
+            }),
 
-			providesTags: cacheUtils.providesList(ApiTags.Boards),
-		}),
+            providesTags: cacheUtils.providesList(ApiTags.Boards),
+        }),
 
-		getBoard: builder.query<iBoard, iBoard["id"]>({
-			query: boardId => ({
-				url: `/boards/${boardId}`,
-				method: "GET",
-			}),
+        getBoard: builder.query<iBoard, iBoard["id"]>({
+            query: boardId => ({
+                url: `/boards/${boardId}`,
+                method: "GET",
+            }),
 
-			providesTags: cacheUtils.cacheByIdArg(ApiTags.Boards),
-		}),
+            providesTags: cacheUtils.cacheByIdArg(ApiTags.Boards),
+        }),
 
-		addBoard: builder.mutation<iBoard, Pick<iBoard, "name" | "authorId">>({
-			query: ({ name, authorId }) => ({
-				url: `/boards`,
-				method: "POST",
-				body: {
-					name,
-					authorId,
-				},
-			}),
+        addBoard: builder.mutation<iBoard, Pick<iBoard, "name" | "authorId">>({
+            query: ({ name, authorId }) => ({
+                url: `/boards`,
+                method: "POST",
+                body: {
+                    name,
+                    authorId,
+                },
+            }),
 
-			invalidatesTags: cacheUtils.invalidatesList(ApiTags.Boards),
-		}),
+            invalidatesTags: cacheUtils.invalidatesList(ApiTags.Boards),
+        }),
 
-		editBoard: builder.mutation<void, Pick<iBoard, "id"> & Partial<Omit<iBoard, "authorId">>>({
-			query: ({ id, name, description }) => ({
-				url: `/boards/${id}`,
-				method: "PUT",
-				body: {
-					name,
-					description,
-				},
-			}),
+        editBoard: builder.mutation<void, Pick<iBoard, "id"> & Partial<Omit<iBoard, "authorId">>>({
+            query: ({ id, name, description }) => ({
+                url: `/boards/${id}`,
+                method: "PUT",
+                body: {
+                    name,
+                    description,
+                },
+            }),
 
-			onQueryStarted: async ({ id, name, description }, { dispatch, queryFulfilled }) => {
-				const patchResult = dispatch(
-					boardService.util.updateQueryData("getUserBoards", id, boards => {
-						const index = boards.findIndex(b => b.id === id);
+            onQueryStarted: async ({ id, name, description }, { dispatch, queryFulfilled }) => {
+                const patchResult = dispatch(
+                    boardService.util.updateQueryData("getUserBoards", id, boards => {
+                        const index = boards.findIndex(b => b.id === id);
 
-						if (index) {
-							name && (boards[index].name = name);
-							description && (boards[index].description = description);
-						}
-					})
-				);
+                        if (index) {
+                            name && (boards[index].name = name);
+                            description && (boards[index].description = description);
+                        }
+                    })
+                );
 
-				try {
-					await queryFulfilled;
-				} catch (e) {
-					patchResult.undo();
-				}
-			},
+                try {
+                    await queryFulfilled;
+                } catch (e) {
+                    patchResult.undo();
+                }
+            },
 
-			invalidatesTags: cacheUtils.invalidatesList(ApiTags.Boards),
-		}),
+            invalidatesTags: cacheUtils.invalidatesList(ApiTags.Boards),
+        }),
 
-		deleteBoard: builder.mutation<void, Pick<iBoard, "id">>({
-			query: ({ id }) => ({
-				url: `/boards/${id}`,
-				method: "DELETE",
-			}),
+        deleteBoard: builder.mutation<void, Pick<iBoard, "id">>({
+            query: ({ id }) => ({
+                url: `/boards/${id}`,
+                method: "DELETE",
+            }),
 
-			onQueryStarted: async ({ id }, { dispatch, queryFulfilled }) => {
-				const patchResult = dispatch(
-					boardService.util.updateQueryData("getUserBoards", id, boards => {
-						const index = boards.findIndex(board => board.id === id);
+            onQueryStarted: async ({ id }, { dispatch, queryFulfilled }) => {
+                const patchResult = dispatch(
+                    boardService.util.updateQueryData("getUserBoards", id, boards => {
+                        const index = boards.findIndex(board => board.id === id);
 
-						if (index) {
-							boards.splice(index, 1);
-						}
-					})
-				);
+                        if (index) {
+                            boards.splice(index, 1);
+                        }
+                    })
+                );
 
-				try {
-					await queryFulfilled;
-				} catch (e) {
-					patchResult.undo();
-				}
-			},
-		}),
-	}),
+                try {
+                    await queryFulfilled;
+                } catch (e) {
+                    patchResult.undo();
+                }
+            },
+
+            invalidatesTags: cacheUtils.invalidatesList(ApiTags.Boards),
+        }),
+    }),
 });
 
 export const {
-	useGetBoardQuery,
-	useAddBoardMutation,
-	useEditBoardMutation,
-	useDeleteBoardMutation,
-	useLazyGetUserBoardsQuery,
-	useGetUserBoardsQuery,
+    useGetBoardQuery,
+    useAddBoardMutation,
+    useEditBoardMutation,
+    useDeleteBoardMutation,
+    useLazyGetUserBoardsQuery,
+    useGetUserBoardsQuery,
 } = boardService;
 
 export default boardService;
