d975398f9755b3ecafdd3675861a527b18219f50
Favian <favouriteteo@gmail.com>
2022-08-21 04:35:42 +0800

    Update editCard modal when folder cards is invalidated

--- src/app/slices/ui/modals.ts
+++ src/app/slices/ui/modals.ts
@@ -1,17 +1,18 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 import { iCard, iFolder } from "../../../utils/models";
+import cardService from "../../services/cards";
 
 interface BaseModalState {
-	open: boolean;
+    open: boolean;
 }
 
 interface EditCardModalState extends BaseModalState {
-	target?: iCard;
+    target?: iCard;
 }
 
 interface EditFolderModalState extends BaseModalState {
-	target?: iFolder;
+    target?: iFolder;
 }
 
 interface EditBoardModalState extends BaseModalState {}
@@ -25,59 +26,76 @@ interface UserSignUpModalState extends BaseModalState {}
 interface ResetPasswordModalState extends BaseModalState {}
 
 type ModalTypes =
-	| "editCard"
-	| "editFolder"
-	| "editBoard"
-	| "addBoard"
-	| "login"
-	| "signup"
-	| "resetPassword";
+    | "editCard"
+    | "editFolder"
+    | "editBoard"
+    | "addBoard"
+    | "login"
+    | "signup"
+    | "resetPassword";
 
 const initialState = {
-	editCard: {
-		open: false,
-	} as EditCardModalState,
-	login: {
-		open: false,
-	} as UserLoginModalState,
-	signup: {
-		open: false,
-	} as UserSignUpModalState,
-	resetPassword: {
-		open: false,
-	} as ResetPasswordModalState,
-	editFolder: {
-		open: false,
-	} as EditFolderModalState,
-	editBoard: {
-		open: false,
-	} as EditBoardModalState,
-	addBoard: {
-		open: false,
-	} as AddBoardModalState,
+    editCard: {
+        open: false,
+    } as EditCardModalState,
+    login: {
+        open: false,
+    } as UserLoginModalState,
+    signup: {
+        open: false,
+    } as UserSignUpModalState,
+    resetPassword: {
+        open: false,
+    } as ResetPasswordModalState,
+    editFolder: {
+        open: false,
+    } as EditFolderModalState,
+    editBoard: {
+        open: false,
+    } as EditBoardModalState,
+    addBoard: {
+        open: false,
+    } as AddBoardModalState,
 };
 
 const modalSlice = createSlice({
-	name: "modals",
-	initialState,
-	reducers: {
-		// TODO: why can't ts infer initialState types???
-		openModal: (state: any, action: PayloadAction<ModalTypes>) => {
-			state[action.payload].open = true;
-		},
+    name: "modals",
+    initialState,
+    reducers: {
+        // TODO: why can't ts infer initialState types???
+        openModal: (state: any, action: PayloadAction<ModalTypes>) => {
+            state[action.payload].open = true;
+        },
 
-		closeModal: (state: any, action: PayloadAction<ModalTypes>) => {
-			state[action.payload].open = false;
-		},
+        closeModal: (state: any, action: PayloadAction<ModalTypes>) => {
+            state[action.payload].open = false;
+        },
 
-		setEditCardTarget: (state: any, action: PayloadAction<iCard>) => {
-			state.editCard.target = action.payload;
-		},
+        setEditCardTarget: (state: any, action: PayloadAction<iCard>) => {
+            state.editCard.target = action.payload;
+        },
 
-		setEditFolderTarget: (state: any, action: PayloadAction<iFolder>) => {
-			state.editFolder.target = action.payload;
-		},
-	},
+        setEditFolderTarget: (state: any, action: PayloadAction<iFolder>) => {
+            state.editFolder.target = action.payload;
+        },
+    },
+
+    extraReducers: builder => {
+        builder.addMatcher(
+            cardService.endpoints.getFolderCards.matchFulfilled,
+            (state, { payload: cards }) => {
+                const currentCard = state.editCard.target;
+                // if there is no current card target, we don't need to do anything
+                if (!currentCard) return;
+
+                const currentCardUpdate = cards.find(card => card.id === currentCard.id);
+
+                if (!currentCardUpdate) return;
+
+                state.editCard.target = currentCardUpdate;
+            }
+        );
+    },
 });
 
 export const { openModal, closeModal, setEditCardTarget, setEditFolderTarget } = modalSlice.actions;
